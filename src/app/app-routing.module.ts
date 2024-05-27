1eda9212f01b9c0fac1f9d426df3f0d189704152
MohamedSaad <ilhsm672016@gmail.com>
2024-05-27 20:36:25 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'auth', loadChildren: () => import('./Modules/auth/auth.module').then(m => m.AuthModule) }, { path: 'landing-page', loadChildren: () => import('./Modules/landing-page/landing-page.module').then(m => m.LandingPageModule) }, { path: 'admin', loadChildren: () => import('./Modules/admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
