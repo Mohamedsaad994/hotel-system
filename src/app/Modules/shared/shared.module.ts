fedba5fd1b3c0cee04d45fc981ac01ce3b16ad3a
MohamedSaad <ilhsm672016@gmail.com>
2024-06-01 14:49:28 +0200

    fix error

--- src/app/Modules/shared/shared.module.ts
+++ src/app/Modules/shared/shared.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeleteComponent } from './components/delete/delete.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { DeleteComponent } from './components/delete/delete.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
