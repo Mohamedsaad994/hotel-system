852653dcb01b58a0cfb71b221af833c0fc87d856
seham <sehamnabawi@gmail.com>
2024-06-01 19:35:00 +0300

    create rooms

--- src/app/Modules/admin/dashboard/modules/rooms/services/rooms.service.ts
+++ src/app/Modules/admin/dashboard/modules/rooms/services/rooms.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IAllRooms } from '../models/rooms';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,9 @@ import { Observable } from 'rxjs';
 export class RoomsService {
 
   constructor(private _HttpClient:HttpClient) { }
-
+getAllRooms(data:any): Observable<IAllRooms>{
+  return this._HttpClient.get<IAllRooms>('admin/rooms' , {params:data})
+}
   AddRoom(roomData:FormData):Observable<any>{
     return this._HttpClient.post('admin/rooms', roomData)
   }
