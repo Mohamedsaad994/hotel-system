ceac4951850434c6a77afe60dfa308e60080c1cd
seham <sehamnabawi@gmail.com>
2024-06-03 21:21:48 +0300

    create booking Table

---
+++ src/app/Modules/admin/dashboard/modules/booking/models/booking.ts
@@ -0,0 +1,33 @@
+export interface IAllBooking {
+    success: boolean;
+    message: string;
+    data: IBookingData;
+  }
+  
+  export interface IBookingData {
+    booking: IBooking[];
+    totalCount: number;
+  }
+  
+  export interface IBooking {
+    _id: string;
+    startDate: string;
+    endDate: string;
+    totalPrice: number;
+    user: IUser;
+    room: IRoom;
+    status: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  export interface IUser {
+    _id: string;
+    userName: string;
+  }
+  
+  export interface IRoom {
+    _id: string
+    roomNumber: string
+  }
+  
