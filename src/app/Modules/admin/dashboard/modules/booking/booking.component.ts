9325dd8b4d8acb211521da8021993b5e672d5645
seham <sehamnabawi@gmail.com>
2024-06-04 17:52:42 +0300

    view booking module

--- src/app/Modules/admin/dashboard/modules/booking/booking.component.ts
+++ src/app/Modules/admin/dashboard/modules/booking/booking.component.ts
@@ -1,20 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IAllBooking, IBooking, IBookingData } from './models/booking';
 import { BookingService } from './services/booking.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { HelperService } from 'src/app/Modules/shared/services/helper.service';
-
+import { ViewListBookingComponent } from './components/view-list-booking/view-list-booking.component';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
-export class BookingComponent {
+export class BookingComponent implements OnInit {
   pageSize: number = 10;
   pageNumber: number = 1;
-
-
-  constructor(private _BookingService:BookingService , private _HelperService:HelperService){}
+  allBooking!: IAllBooking;
+  BookingData: any[]=[];
+  tableResponse: IBookingData|undefined;
+ 
+ constructor(private _BookingService:BookingService , 
+  private _HelperService:HelperService ,
+  public dialog:MatDialog){}
 
   ngOnInit(): void {
     this.onGetAllBooking()
@@ -29,17 +34,12 @@ export class BookingComponent {
     userName: 'User',
     actions: 'Actions',
   };
-  allBooking!: IAllBooking;
-  BookingData: any[]=[];
-  tableResponse: IBookingData|undefined;
- 
   onGetAllBooking(){
     const params = {
       pageSize: this.pageSize,
       pageNumber: this.pageNumber,
     };
-
-    this._BookingService.getAllBooking(params).subscribe({
+   this._BookingService.getAllBooking(params).subscribe({
       next: (res) => {
         this.tableResponse = res.data;
        this.BookingData = this.tableResponse?.booking.map(ad => ({
@@ -52,24 +52,45 @@ export class BookingComponent {
        }) )
       },
       error: (err: HttpErrorResponse) => this._HelperService.error(err),
-      complete: () => this._HelperService.success('Booking have been Retrieved Successfully'),
+      complete: () => {},
     });
 
   }
 
 
 
+  onGetBookingDetailsById(id:string , userData:any){
+    this._BookingService.getBookingDetailsById(id).subscribe({
+      next:(res)=>{
+       console.log(res);
+       
+      },
+    })
+}
 
 
+  openViewBookingDialog(id:string): void {
+    const dialogRef = this.dialog.open(ViewListBookingComponent, {
+      width: '550px',
+      height: 'auto',
+      data: {},
+    });
 
+    dialogRef.afterClosed().subscribe((result) => {
+     this.onGetBookingDetailsById(id ,result)
+    });
+  }
 
 handleViewItem(id:string){
-  console.log(id);
+ this.openViewBookingDialog(id)
 }
-handleEditItem(id:string){
-  console.log(id);
+
+
+
+
+
+
+
+
 }
-handleDeleteItem(id:string){
-  console.log(id);
-}
-}
+
